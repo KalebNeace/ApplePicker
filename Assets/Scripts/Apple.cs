f4d776f7de07abb7795430e11ed86094152ec9f5
Shreejan Pandey <81029735+NeplayGames@users.noreply.github.com>
2024-09-17 00:02:19 -0500

    Some suggestion to fix the bug

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -7,12 +7,17 @@ public class Apple : MonoBehaviour
     public static float bottomY = -20f;
 
     // Update is called once per frame
-    void Update()
+       void Update()
     {
         if (transform.position.y < bottomY){
             Destroy(this.gameObject);
         }
         ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+        //ToDo: 
+        //This seems to be called from wrong place. 
+        //Make sure that this is called on appropriate time and not every frame.
+        //I believe this should be inside the if function {}
         apScript.AppleMissed();
     }
+
 }
