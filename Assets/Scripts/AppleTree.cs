ce4d8d6b79886dda769d8890bea8f89a9597cd27
KalebNeace <kaleb.neace@gmail.com>
2024-09-17 17:57:05 -0500

    Added Main Menu screen and Game Over screen

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -5,6 +5,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.Assertions;
 
+
 public class AppleTree : MonoBehaviour
 { 
     [Header("Inscribed")]
@@ -15,13 +16,14 @@ public class AppleTree : MonoBehaviour
     public float leftAndRightEdge = 10f; //Distance where apple tree turns around
     public float changeDirChance = 0.1f; //Chance apple tree will change directions
     public float appleDropDelay = 2f; //Seconds between apples instantiations
+    public float rottenAppleChance = 0.1f;
 
     void Start()
     {
       Invoke("DropApple", 2f);
     }
     void DropApple(){
-      GameObject apple = Instantiate<GameObject>(Apple);
+      GameObject apple = Instantiate<GameObject>(RandomApple());
       apple.transform.position = transform.position;
       Invoke("DropApple", appleDropDelay);
     }
@@ -43,5 +45,13 @@ public class AppleTree : MonoBehaviour
     if (Random.value < changeDirChance){
         speed *= -1; //Change direction   
     }
+  }
+  GameObject RandomApple(){
+    float RanNumber = Random.Range(0f,1f);
+    if (RanNumber < rottenAppleChance){
+      return RottenApple;
+    }
+    else
+      return Apple;
   }
 }
